6d6faaa22e9508b99d6ddce9ae2e896e568ba8c7
Rigent Tio Salma <rigentjogja@gmail.com>
2024-04-03 15:40:45 +0700

    make infinity scroll and clean up function

--- client/src/pages/branch.jsx
+++ client/src/pages/branch.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect, useState } from "react";
+import React, { Suspense, lazy, useCallback, useEffect, useRef, useState } from "react";
 import SidebarBranchLayout from "../components/Layouts/SidebarBranchLayout";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { axiosInstance } from "../lib/axios";
@@ -12,47 +12,149 @@ import ProductNotFound from "../components/Elements/NotFound/ProductNotFound";
 // const CardProduct = lazy(() => import("../components/Fragments/Card/CardProduct"));
 
 const BranchPage = () => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
   const [userBookmarks, setUserBookmarks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { branchSlug, categorySlug, subCategorySlug } = useParams();
   const { isLoggedIn } = useAuth();
   const cookie = new Cookies();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (branchSlug && categorySlug && subCategorySlug) {
-          const response = await axiosInstance.get(`/product/get_by_subcategory/${branchSlug}/${categorySlug}/${subCategorySlug}`);
-          setProducts(response.data.data);
-        } else if (branchSlug && categorySlug) {
-          const response = await axiosInstance.get(`/product/get_by_category/${branchSlug}/${categorySlug}`);
-          setProducts(response.data.data);
-        } else if (branchSlug) {
-          const response = await axiosInstance.get(`/product/get_by_branch/${branchSlug}`);
-          setProducts(response.data.data);
-        }
+  const intObserver = useRef();
 
-        if (isLoggedIn) {
-          const response = await axiosInstance.get("/bookmark", {
-            headers: {
-              Authorization: `Bearer ${cookie.get("access_token")}`,
-            },
-          });
-          setUserBookmarks(response.data.data);
+  const listEndRef = useCallback(
+    (node) => {
+      if (isLoading || !hasNextPage) return;
+
+      if (intObserver.current) intObserver.current.disconnect();
+      intObserver.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          setPage((prevPage) => prevPage + 1);
         }
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
+      });
+      if (node) intObserver.current.observe(node);
+    },
+    [isLoading, hasNextPage]
+  );
+
+  const fetchData = async (signal, currentPage) => {
+    try {
+      setIsLoading(true);
+
+      if (isLoggedIn) {
+        const response = await axiosInstance.get("/bookmark", {
+          headers: {
+            Authorization: `Bearer ${cookie.get("access_token")}`,
+          },
+          signal,
+        });
+        setUserBookmarks(response.data.data);
       }
-    };
 
-    fetchData();
+      let fetchPage = currentPage;
+      if (branchSlug && categorySlug && subCategorySlug) {
+        const response = await axiosInstance.get(`/product/get_by_subcategory/${branchSlug}/${categorySlug}/${subCategorySlug}?page=${fetchPage}&perPage=10`, { signal });
+        setHasNextPage(response.data.data.next_page_url !== null);
+        setProducts((prevProducts) => {
+          return [...new Set([...prevProducts, ...response.data.data.data])];
+        });
+      } else if (branchSlug && categorySlug) {
+        const response = await axiosInstance.get(`/product/get_by_category/${branchSlug}/${categorySlug}?page=${fetchPage}&perPage=10`, { signal });
+        setHasNextPage(response.data.data.next_page_url !== null);
+        setProducts((prevProducts) => {
+          return [...new Set([...prevProducts, ...response.data.data.data])];
+        });
+      } else if (branchSlug) {
+        const response = await axiosInstance.get(`/product/get_by_branch/${branchSlug}?page=${fetchPage}&perPage=10`, { signal });
+        setHasNextPage(response.data.data.next_page_url !== null);
+        setProducts((prevProducts) => {
+          return [...new Set([...prevProducts, ...response.data.data.data])];
+        });
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    setPage(1);
+    setProducts([]);
+    setHasNextPage(true);
+
+    fetchData(signal, 1);
+
+    return () => controller.abort();
   }, [branchSlug, categorySlug, subCategorySlug, isLoggedIn]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    if (page > 1) {
+      fetchData(signal, page);
+    }
+
+    return () => controller.abort();
+  }, [page]);
 
   const isProductBookmarked = (productId) => {
     return userBookmarks.some((bookmark) => bookmark.pivot.product_id === productId);
   };
+
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   const { signal } = controller;
+
+  //   setPage(1);
+  //   setProducts([]);
+  //   setHasNextPage(true);
+
+  //   const fetchData = async () => {
+  //     setIsLoading(true);
+  //     try {
+  //       if (branchSlug && categorySlug && subCategorySlug) {
+  //         const response = await axiosInstance.get(`/product/get_by_subcategory/${branchSlug}/${categorySlug}/${subCategorySlug}?page=${page}&perPage=10`, { signal });
+  //         setHasNextPage(response.data.data.next_page_url !== null);
+  //         setProducts((prevProducts) => {
+  //           return [...new Set([...prevProducts, ...response.data.data.data])];
+  //         });
+  //       } else if (branchSlug && categorySlug) {
+  //         const response = await axiosInstance.get(`/product/get_by_category/${branchSlug}/${categorySlug}?page=${page}&perPage=10`, { signal });
+  //         setHasNextPage(response.data.data.next_page_url !== null);
+  //         setProducts((prevProducts) => {
+  //           return [...new Set([...prevProducts, ...response.data.data.data])];
+  //         });
+  //       } else if (branchSlug) {
+  //         const response = await axiosInstance.get(`/product/get_by_branch/${branchSlug}?page=${page}&perPage=10`, { signal });
+  //         setHasNextPage(response.data.data.next_page_url !== null);
+  //         setProducts((prevProducts) => {
+  //           return [...new Set([...prevProducts, ...response.data.data.data])];
+  //         });
+  //       }
+
+  //       if (isLoggedIn) {
+  //         const response = await axiosInstance.get("/bookmark", {
+  //           headers: {
+  //             Authorization: `Bearer ${cookie.get("access_token")}`,
+  //           },
+  //         });
+  //         setUserBookmarks(response.data.data);
+  //       }
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchData(signal);
+
+  //   return () => controller.abort();
+  // }, [branchSlug, categorySlug, subCategorySlug, isLoggedIn]);
 
   return (
     <SidebarBranchLayout>
@@ -73,17 +175,21 @@ const BranchPage = () => {
           </div>
         </div>
         <div className="grid md:grid-cols-1 lg:grid-cols-3 gap-6 ">
-          {isLoading ? (
-            <CardSkeleton count={3} />
-          ) : products.length > 0 ? (
-            products.map((product) => (
-              <CardProduct key={product.id} isBookmarked={isProductBookmarked(product.id)} productId={product.id} image={product.thumbnail} stock={product.is_stock} branch={product.branch.name} price={product.price} to={product.slug}>
-                {product.name}
-              </CardProduct>
+          {products.length > 0 ? (
+            products.map((product, index) => (
+              <React.Fragment key={product.id}>
+                <CardProduct key={product.id} isBookmarked={isProductBookmarked(product.id)} productId={product.id} image={product.thumbnail} stock={product.is_stock} branch={product.branch.name} price={product.price} to={product.slug}>
+                  {product.name}
+                </CardProduct>
+                {index === products.length - 1 && <div ref={listEndRef}></div>}
+              </React.Fragment>
             ))
+          ) : isLoading ? (
+            <CardSkeleton count={3} />
           ) : (
             <ProductNotFound />
           )}
+          {isLoading && <CardSkeleton count={3} />}
         </div>
       </div>
     </SidebarBranchLayout>
